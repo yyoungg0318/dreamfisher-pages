6b4c486a0c390b68a982c7b85e50efe3f9ea76cf
benson.yang <yyoungg0318@gmail.com>
2021-01-26 10:41:06 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5e39ab3091849cd0c17b";
+/******/ 	var hotCurrentHash = "ebad0b4c6704f14d2da5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
